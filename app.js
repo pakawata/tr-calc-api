ed8e2d43a75e2a64c871ea2618f9bf5cb7b30e9e
Pakawat <pakhawat_a@hotmail.com>
2018-01-28 11:57:02 +0700

    remove unused route

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 const AWS = require('aws-sdk');
 
 AWS.config.update({
